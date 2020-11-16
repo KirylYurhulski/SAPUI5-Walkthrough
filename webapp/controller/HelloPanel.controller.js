094a849e35c6210d2f270c091508a3e824f94ea4
Kiryl <kirill.yurgulskij88@gmail.com>
2020-11-16 15:01:27 +0300

    Step 16: Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment) {
 	"use strict";
 
 	return Controller.extend("walkthrough.controller.HelloPanel", {
@@ -11,6 +12,24 @@ sap.ui.define([
 					this.getView().getModel().getProperty("/recipient/name")
 				])
 			);
-        }
+		},
+
+		onOpenDialog: function(){
+			let oView = this.getView();
+			// create dialog lazily
+			if (!this.byId("helloDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "walkthrough.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			}
+		}
 	});
 });
