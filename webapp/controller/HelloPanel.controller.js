642a4fcf09f947980b05fb33043282db2af838a0
Kiryl <kirill.yurgulskij88@gmail.com>
2020-11-17 12:16:57 +0300

    Step 22: Expression Binding

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("walkthrough.controller.HelloPanel", {
-        onShowHello : function () {
+		onShowHello : function () {
             MessageToast.show(
 				this.getView().getModel("i18n").getResourceBundle().getText("helloMsg", [
 					this.getView().getModel().getProperty("/recipient/name")
